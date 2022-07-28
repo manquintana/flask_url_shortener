8b3898ceb130b2c82383509f6f7509b794772878
Manuel Alejandro Quintana <manuelalejandroquintana@gmail.com>
2022-07-28 16:52:54 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,54 @@
+import json
+import os.path
+from flask import Flask, render_template, request, redirect, url_for, flash, abort
+from werkzeug.utils import secure_filename #Secure in filename
+
+app = Flask(__name__) #name of the module running in flask
+app.secret_key = 'keyformessagesfasfasf312r1412'
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/your-url', methods=['GET', 'POST'])
+def your_url():
+    if request.method == 'POST':
+        urls = {}
+        
+        if os.path.exists('urls.json'):
+            with open('urls.json') as urls_file:
+                urls = json.load(urls_file)
+                
+        if request.form['code'] in urls.keys():
+            flash('That short name has already been taken! Please select another name.')
+            return redirect(url_for('home'))
+        
+        if 'url' in request.form.keys():
+            urls[request.form['code']] = {'url': request.form['url']}
+        else:
+            f = request.files['file']
+            full_name = secure_filename(f.filename)
+            f.save(f'static/user_files/{full_name}')
+            urls[request.form['code']] = {'file': full_name}
+        with open('urls.json', 'w') as url_file:
+            json.dump(urls, url_file)
+            return render_template('your_url.html', code=request.form['code']) # request.form para usar POST //request.args para usar GET
+    else:
+        return redirect(url_for('home'))
+
+@app.route('/<string:code>')
+def redirect_to_url(code):
+    if os.path.exists('urls.json'):
+        with open('urls.json') as urls_file:
+            urls = json.load(urls_file)
+            if code in urls.keys():
+                if 'url' in urls[code].keys():# Es una url
+                    return redirect(urls[code]['url'])
+                else:
+                   return redirect(url_for('static', filename=f'user_files/{urls[code]["file"]}'))
+    return abort(404)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html'), 404
+    
